7022d3c819adb663629e56b078936eb78ad0b16e
Abdol Reza (Martin) Askari <149413362+martinaskari13581120@users.noreply.github.com>
2026-09-14 21:23:52 -0600

    Sarah: initial login screen - set StatusBar light

--- App.tsx
+++ App.tsx
@@ -25,7 +25,7 @@ export default function App() {
     } else {
         return (
             <>
-                <StatusBar animated translucent style="dark" />
+                <StatusBar animated translucent style="light" />
                 <ActionSheetProvider>
                     <AppStack />
                 </ActionSheetProvider>
